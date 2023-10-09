1366764cab2a36bb40d140986595cb691ec8840a
Vei <alu0101563428@ull.edu.es>
2023-10-09 09:17:31 -0400

    Empty comment

---
+++ IB/Jutge/Alternatives_and_iterations/P59875_Top_to_bottom.cc
@@ -0,0 +1,39 @@
+/**
+ * Universidad de La Laguna
+ * Escuela Superior de Ingeniería y Tecnología
+ * Grado en Ingeniería Informática
+ * Informática Básica 2023-2024
+ * 
+ * @author  Javier Rodríguez González (alu0101563428@ull.edu.es)
+ * @date    9 Oct. 2023
+ * @brief   This program takes two numbers as its input, and prints
+ * 	    out all the numbers comprised between the first and the
+ * 	    second one in decreasing order.
+ * @bug	    There are no known bugs.
+ * @see	    https://jutge.org/problems/P59875
+ */
+
+#include <iostream>
+
+int main() {
+  int num1;
+  int num2;
+  while (std::cin >> num1 >> num2)
+  {
+    if (num1 >= num2)
+    {
+      for (int i = num1; i >= num2; i--)
+      {
+        std::cout << i << std::endl;
+      }
+    }
+    else
+    {
+      for (int i = num2; i >= num1; i--)
+      {
+        std::cout << i << std::endl;
+      }
+    }
+  }
+  return 0;
+}
