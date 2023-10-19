a71a9a0131adc5c7d393e98b98ab076133dc69bb
Vei <alu0101563428@ull.edu.es>
2023-10-19 04:32:36 -0400

    optimization

--- Jutge/IB/Alternatives_and_iterations/P81629_Minimum_change.cc
+++ Jutge/IB/Alternatives_and_iterations/P81629_Minimum_change.cc
@@ -21,35 +21,20 @@ int main()
   while (std::cin >> euros >> cents)
   {
     std::cout << "Banknotes of 500 euros: " << euros / 500 << std::endl;
-    euros %= 500;
-    std::cout << "Banknotes of 200 euros: " << euros / 200 << std::endl;
-    euros %= 200;
-    std::cout << "Banknotes of 100 euros: " << euros / 100 << std::endl;
-    euros %= 100;
-    std::cout << "Banknotes of 50 euros: " << euros / 50 << std::endl;
-    euros %= 50;
-    std::cout << "Banknotes of 20 euros: " << euros / 20 << std::endl;
-    euros %= 20;
-    std::cout << "Banknotes of 10 euros: " << euros / 10 << std::endl;
-    euros %= 10;
-    std::cout << "Banknotes of 5 euros: " << euros / 5 << std::endl;
-    euros %= 5;
-    std::cout << "Coins of 2 euros: " << euros / 2 << std::endl;
-    euros %= 2;
-    std::cout << "Coins of 1 euro: " << euros / 1 << std::endl;
-    euros %= 1;
+    std::cout << "Banknotes of 200 euros: " << (euros %= 500) / 200 << std::endl;
+    std::cout << "Banknotes of 100 euros: " << (euros %= 200) / 100 << std::endl;
+    std::cout << "Banknotes of 50 euros: " << (euros %= 100) / 50 << std::endl;
+    std::cout << "Banknotes of 20 euros: " << (euros %= 50) / 20 << std::endl;
+    std::cout << "Banknotes of 10 euros: " << (euros %= 20) / 10 << std::endl;
+    std::cout << "Banknotes of 5 euros: " << (euros %= 10) / 5 << std::endl;
+    std::cout << "Coins of 2 euros: " << (euros %= 5) / 2 << std::endl;
+    std::cout << "Coins of 1 euro: " << (euros %= 2) / 1 << std::endl;
     std::cout << "Coins of 50 cents: " << cents / 50 << std::endl;
-    cents %= 50;
-    std::cout << "Coins of 20 cents: " << cents / 20 << std::endl;
-    cents %= 20;
-    std::cout << "Coins of 10 cents: " << cents / 10 << std::endl;
-    cents %= 10;
-    std::cout << "Coins of 5 cents: " << cents / 5 << std::endl;
-    cents %= 5;
-    std::cout << "Coins of 2 cents: " << cents / 2 << std::endl;
-    cents %= 2;
-    std::cout << "Coins of 1 cent: " << cents / 1 << std::endl;
-    cents %= 1;
+    std::cout << "Coins of 20 cents: " << (cents %= 50) / 20 << std::endl;
+    std::cout << "Coins of 10 cents: " << (cents %= 20) / 10 << std::endl;
+    std::cout << "Coins of 5 cents: " << (cents %= 10) / 5 << std::endl;
+    std::cout << "Coins of 2 cents: " << (cents %= 5) / 2 << std::endl;
+    std::cout << "Coins of 1 cent: " << (cents %= 2) / 1 << std::endl;
   }
   return 0;
 }
