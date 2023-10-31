50d2a701dc62c49c9460dc79939418cb031ad1fb
Vei <alu0101563428@ull.edu.es>
2023-10-31 16:49:52 +0000

    Program changes

--- Jutge/IB/Alternatives_and_iterations/P97156_Numbers_in_an_interval.cc
+++ Jutge/IB/Alternatives_and_iterations/P97156_Numbers_in_an_interval.cc
@@ -6,9 +6,32 @@
  * 
  * @file   P97156_Numbers_in_an_interval.cc
  * @author Javier Rodríguez González (alu0101563428@ull.edu.es)
- * @date   16 Oct. 2023
- * @brief  
+ * @date   22 Oct. 2023
+ * @brief  This program takes two numbers, 'num1' and 'num2', and prints all numbers between said given numbers separated with commas.
  * @bug    There are no known bugs.
  * @see    https://jutge.org/problems/P97156
  */
 
+#include <iostream>
+
+int main()
+{
+  int num1;
+  int num2;
+  while (std::cin >> num1 >> num2)
+  {
+    if (num1 > num2)
+    {
+      std::cout << std::endl;
+    }
+    else
+    {
+      for (int i = num1; i < num2; i++)
+        {
+          std::cout << i << ",";
+        }
+      std::cout << num2 << std::endl;
+    }
+  }
+  return 0;
+}
